03e8cb150511db8312c21bf44e3749dd6187548d
Hussein <hznagri@uwaterloo.ca>
2019-09-17 09:54:34 -0400

    initial commit

---
+++ StormViewer/StormViewer/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  StormViewer
+//
+//  Created by Hussein Nagri on 2019-09-16.
+//  Copyright © 2019 Hussein Nagri. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
