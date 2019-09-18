b44c2ce71bd76cf32d3f0408f290acb170c86d11
Hussein <hznagri@uwaterloo.ca>
2019-09-18 15:37:21 -0400

    added forward and back button

--- Easy Browser/Easy Browser/ViewController.swift
+++ Easy Browser/Easy Browser/ViewController.swift
@@ -11,6 +11,9 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView : WKWebView!
+    var progressView : UIProgressView!
+    var websites = ["google.ca", "apple.com", "facebook.com"]
+    
     
     override func loadView() {
         webView = WKWebView()
@@ -22,11 +25,83 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let url = URL(string: "https://google.ca")!
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        let goBack = UIBarButtonItem(title: "Back", style: .plain, target: webView, action: #selector(webView.goBack))
+        let goForward = UIBarButtonItem(title: "Forward", style: .plain, target: webView, action: #selector(webView.goForward))
+        
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        
+        
+        toolbarItems = [goBack, progressButton, space, refresh, goForward]
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+        
+        
+        let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
+    
+    @objc func openTapped(){
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        for website in websites{
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        //we type this for ipads: it tells ios where to keep the action sheet be ankored
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction){
+        /*
+        can also write the next statement with a guard let to be safe..
+         guard let actionTitle = action.title else {return}
+         guard let url = URL(string: "https://" + actionTitle) else {return}
+ 
+ */
+        
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
 
-
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress"{
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url //set the url of navigation as a constant
+        
+        if let host  = url?.host { //unwrap urls with a host
+            for website in websites{
+                if host.contains(website){
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        let invalidPage = UIAlertController(title: "Error", message: "You are not allowed to access this site", preferredStyle: .alert)
+        
+        invalidPage.addAction(UIAlertAction(title: "Back", style: .default, handler: nil))
+        
+        //we type this for ipads: it tells ios where to keep the action sheet be ankored
+        invalidPage.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(invalidPage, animated: true)
+        decisionHandler(.cancel)
+    }
 }
+
 
