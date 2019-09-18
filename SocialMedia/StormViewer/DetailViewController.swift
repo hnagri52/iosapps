a64a1daa67e48b5054a5d38dafbab4606c0bc9ca
Hussein <hznagri@uwaterloo.ca>
2019-09-17 21:08:06 -0400

    created a simple knowledge game

---
+++ SocialMedia/StormViewer/DetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.swift
+//  StormViewer
+//
+//  Created by Hussein Nagri on 2019-09-16.
+//  Copyright © 2019 Hussein Nagri. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var selectedPictureNumber: Int = 0
+    var totalPictures:Int = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "This is \(selectedPictureNumber) of \(totalPictures)"
+        navigationItem.largeTitleDisplayMode = .never 
+        
+        if let imageToLoad = selectedImage{
+            imageView.image = UIImage(named: imageToLoad)
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
