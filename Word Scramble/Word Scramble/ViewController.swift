4b91625f31b2e6009e2718f7a71a5d675f3804d5
Hussein <hznagri@uwaterloo.ca>
2019-09-20 09:00:20 -0400

    started word scramble

---
+++ Word Scramble/Word Scramble/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Word Scramble
+//
+//  Created by Hussein Nagri on 2019-09-18.
+//  Copyright © 2019 Hussein Nagri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startWords = try?String(contentsOf: startWordsURL){
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        if allWords.isEmpty{
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+    
+    func startGame (){
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+
+
+}
+
